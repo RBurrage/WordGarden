5641635c812fafbbb97c12d1a2496449b06b7317
Robin Burrage <robin.o.burrage@gmail.com>
2026-03-19 15:38:06 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Robin Burrage on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
